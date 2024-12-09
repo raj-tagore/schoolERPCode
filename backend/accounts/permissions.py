b4c48d4baffd853e77007d99f2587a20ff6f60a5
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-12-09 17:31:59 +0530

    permissions for accounts

--- backend/accounts/permissions.py
+++ backend/accounts/permissions.py
@@ -2,6 +2,6 @@ import rest_framework.permissions import BasePermissions
 
 class AccountViewSetPermissions(BasePermissions):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'POST']:
-            return request.is_staff
-        return request.user.username == obj.username
+        if request.method in ['GET']:
+            return True
+        return request.user.username == obj.username && request.is_staff && request.user.is_approved
