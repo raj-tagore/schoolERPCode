d26029fd4083bb467f31e51f14017fdff783b1a6
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-12-10 18:18:29 +0530

    Some UI changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
 <template>
   <header>
     <div class="container">
-      <spam>The App</spam>
+      <span>The App</span>
       <nav>
         <ul>
           <li><a class="secondary" href="#">About</a></li>
@@ -16,16 +16,17 @@
     </div>
   </header>
   <main class="container">
-    <aside class="container" id="table-of-contents">
+	<a class="secondary" id="sidebarOpener" :class="{shown: !isSidebarOpen}" @click="isSidebarOpen = true" href="#">Table of Contents</a>
+	<aside class="container" id="table-of-contents" :class="{open: isSidebarOpen}">
       <header>
           <h2>Table of Contents</h2>
-          <a class="secondary" href="#">Close</a>
+          <a class="secondary" @click="isSidebarOpen = false" href="#">Close</a>
       </header>
       <nav>
         <ul>
-          <li><a class="secondary" href="#">About</a></li>
-          <li><a class="secondary" href="#">Services</a></li>
-          <li><a class="secondary" href="#">Products</a></li>
+          <li v-for="item in tableOfContents" :key="item.id">
+			<a class="secondary" :href="'#' + item.id">{{ item.text }}</a>
+          </li>
         </ul>
       </nav>
     </aside>
@@ -56,24 +57,47 @@ header > .container {
   align-items: center;
 }
 
+.container:has(#table-of-contents) > a {
+  display: none;
+}
+
 @media (max-width: 1023px) {
   main {
     grid-template-columns: 1fr;
   }
+
   main > #table-of-contents {
     background-color: var(--pico-background-color);
     padding-top: 30px;
     position: fixed;
 	top: 0%;
-	left: 0%;
+	left: -100%;
     width: 100%;
     height: 100%;
+    padding: 20px;
+    transition: left 0.5s ease-in-out;
+    box-shadow: rgba(0, 0, 0, 0.1) 15px 0px 40px -3px
+  }
+
+  main > #table-of-contents.open {
+    left: 0;
+  }
+
+  #table-of-contents > header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 
   #table-of-contents > header > h2 {
     display: block;
   }
+
+  .container:has(#table-of-contents) > a {
+    display: block;
+  }
 }
+
 
 </style>
 
@@ -81,4 +105,45 @@ header > .container {
 export default {
   name: 'App',
 }
+</script>
+<script setup>
+import {ref, onMounted} from "vue";
+
+const isSidebarOpen = ref(false)
+
+const tableOfContents = ref([]);
+
+onMounted(() => {
+  generateTableOfContents();
+	console.log("Mounted")
+});
+
+function generateTableOfContents() {
+	console.log("Generating Table of Contents")
+  const headings = document.getElementById("content").querySelectorAll('h1, h2, h3, h4, h5, h6');
+	console.log(headings);
+  
+  // Map headings into a structured array
+	tableOfContents.value = Array.from(headings).map((heading) => {
+		let textContent = heading.textContent;
+		if (textContent.length > 15) {
+			textContent = textContent.substring(0, 15) + "...";
+		}
+		return {
+		id: heading.id || generateId(heading.textContent), // Use existing ID or generate one
+		text: textContent,
+		level: heading.tagName.toLowerCase(), // h1, h2, etc.
+	}
+	});
+
+  headings.forEach((heading) => {
+    if (!heading.id) heading.id = generateId(heading.textContent);
+  });
+}
+
+function generateId(text) {
+  return text.toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]/g, '');
+}
+
+
 </script>
