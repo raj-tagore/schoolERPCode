16bfd1581fbe60adf1f969102a74dbd8f16a6c88
Raj Tagore <rajtagore780@gmail.com>
2024-12-11 23:54:49 +0530

    blank=true on teachers in classrooms and subjects

--- backend/allocation/models.py
+++ backend/allocation/models.py
@@ -7,9 +7,9 @@ class Classroom(models.Model):
     name = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     standard = models.CharField(max_length=50)
-    students = models.ManyToManyField(Account, related_name='classrooms', limit_choices_to={'groups__name': 'Student'})
+    students = models.ManyToManyField(Account, related_name='classrooms', limit_choices_to={'groups__name': 'Student'}, blank=True)
     class_teacher = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='classrooms_leading', limit_choices_to={'groups__name': 'Teacher'})
-    other_teachers = models.ManyToManyField(Account, related_name='classrooms_assisting', limit_choices_to={'groups__name': 'Teacher'})
+    other_teachers = models.ManyToManyField(Account, related_name='classrooms_assisting', limit_choices_to={'groups__name': 'Teacher'}, blank=True)
 
     def __str__(self):
         return self.name
@@ -20,8 +20,8 @@ class Subject(models.Model):
     description = models.TextField()
     classroom = models.ForeignKey(Classroom, on_delete=models.SET_NULL, null=True, related_name='subjects')
     main_teacher = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='subjects_leading', limit_choices_to={'groups__name': 'Teacher'})
-    other_teachers = models.ManyToManyField(Account, related_name='subjects_assisting', limit_choices_to={'groups__name': 'Teacher'})
-    additional_students = models.ManyToManyField(Account, related_name='subjects', limit_choices_to={'groups__name': 'Student'})
+    other_teachers = models.ManyToManyField(Account, related_name='subjects_assisting', limit_choices_to={'groups__name': 'Teacher'}, blank=True)
+    additional_students = models.ManyToManyField(Account, related_name='subjects', limit_choices_to={'groups__name': 'Student'}, blank=True)
     
     def __str__(self):
         return self.name
