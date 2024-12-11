b54237daec638bf92c1e8f4c882cec2dfd7365b9
Raj Tagore <rajtagore780@gmail.com>
2024-12-11 23:01:57 +0530

    better navbar

--- frontend/src/layouts/DashboardLayout.vue
+++ frontend/src/layouts/DashboardLayout.vue
@@ -2,36 +2,49 @@
   <v-app>
     <v-navigation-drawer app v-model="drawer" color="grey lighten-4">
       <v-list dense>
+        <!-- User Info -->
         <v-list-item>
-          <h3>
-            {{ user.first_name }} {{ user.last_name}}
-          </h3>
-          <h4>
-            {{ user.username }}
-          </h4>
-          <span>
-            {{userType}}
-          </span>
+          <div>
+            <h3>{{ user.first_name }} {{ user.last_name }}</h3>
+            <h4>{{ user.username }}</h4>
+            <span>{{ userType }}</span>
+          </div>
         </v-list-item>
-        <v-divider
-          :thickness="10"
-          class="border-opacity-100"
-        ></v-divider>
+
+        <v-divider :thickness="10" class="border-opacity-100"></v-divider>
+
+        <!-- Dashboard -->
         <v-list-item to="/dashboard">
           <v-list-item-title>Dashboard</v-list-item-title>
         </v-list-item>
+
+        <!-- Create User -->
         <v-list-item to="/register">
           <v-list-item-title>Create User</v-list-item-title>
         </v-list-item>
 
-        <v-list-item to="/announcement">
+        <!-- Announcements Toggle -->
+        <v-list-item @click="announcementsOpen = !announcementsOpen" class="cursor-pointer">
           <v-list-item-title>Announcements</v-list-item-title>
+          <v-spacer></v-spacer>
+          <v-icon>{{ announcementsOpen ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
         </v-list-item>
-        <v-list-item to="/announcement/create">
-          <v-list-item-title>Create Announcement</v-list-item-title>
-        </v-list-item>
+
+        <!-- Nested Items (Shown/Hidden with v-expand-transition) -->
+        <v-expand-transition>
+          <div v-if="announcementsOpen" style="padding-left: 24px;">
+            <v-list-item to="/announcement">
+              <v-list-item-title>View Announcements</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/announcement/create">
+              <v-list-item-title>Create Announcement</v-list-item-title>
+            </v-list-item>
+          </div>
+        </v-expand-transition>
+
       </v-list>
     </v-navigation-drawer>
+
 
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -58,6 +71,7 @@ export default {
   data() {
     return {
       drawer: false,
+      announcementsOpen: false,
     };
   },
   methods: {
