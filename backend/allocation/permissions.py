3b68e6d9f33ef3b80ad81ec9a14d16bd207bf680
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-12-16 20:12:33 +0530

    classroom permissions

---
+++ backend/allocation/permissions.py
@@ -0,0 +1,48 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.contrib.auth.models import Group
+
+class ClassroomPermissions(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.user.is_superuser:
+                return True
+        
+        if "Admin" in [group.name for group in request.user.groups.all()]:
+            return True
+        
+        # Check if user is in the 'Teacher' group
+        if "Teacher" in [group.name for group in request.user.groups.all()]:
+            # Check if user is teaching or assisting in any related classroom
+            classrooms = obj.classrooms.all()
+            if obj.class_teacher == request.user or obj.other_teachers.filter(id=request.user.id).exists():
+                return True
+            if obj.students.filter(id=request.user.id).exists() and request.method in SAFE_METHODS:
+                return True
+        return False
+    
+
+class SubjectPermissions(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.user.is_superuser:
+                return True
+        
+        if "Admin" in [group.name for group in request.user.groups.all()]:
+            return True
+        
+        # Check if user is in the 'Teacher' group
+        if "Teacher" in [group.name for group in request.user.groups.all()]:
+            # Check if user is teaching or assisting in any related classroom
+            classrooms = obj.classrooms.all()
+            if classrooms.filter(class_teacher=request.user).exists() or \
+               classrooms.filter(other_teachers=request.user).exists():
+                return True
+
+            if obj.students.filter(id=request.user.id).exists() and request.method in SAFE_METHODS:
+                return True
+            return False
+        
+        return False
+    
