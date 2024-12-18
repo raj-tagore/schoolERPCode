092c96425fbf1addcab447bedc5cd22b881078bb
Raj Tagore <rajtagore780@gmail.com>
2024-12-19 04:08:50 +0530

    --

--- frontend/src/views/DashboardPage.vue
+++ frontend/src/views/DashboardPage.vue
@@ -1,27 +1,3 @@
 <template>
-      <v-container>
-        <v-tabs v-model="activeTab" mandatory>
-          <v-tab value="dashboard">Dashboard</v-tab>
-          <v-tab value="profile">Profile</v-tab>
-          <v-tab value="settings">Settings</v-tab>
-        </v-tabs>
-
-        <v-tabs-items v-model="activeTab">
-          <v-tab-item value="dashboard">
-            <p>Welcome to the Dashboard!</p>
-          </v-tab-item>
-          <v-tab-item value="profile">
-            <p>Here is your Profile information.</p>
-          </v-tab-item>
-          <v-tab-item value="settings">
-            <p>Adjust your Settings here.</p>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-container>
+  Hello World
 </template>
-
-<script setup>
-import { ref } from 'vue'
-
-const activeTab = ref('dashboard')
-</script>
