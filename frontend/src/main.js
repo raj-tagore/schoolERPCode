238e9ca14ea8a7a62e2f8a26e3019537492abadc
Raj Tagore <rajtagore780@gmail.com>
2024-12-22 23:33:42 +0530

    --

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPersistedState from 'pinia-plugin-persistedstate'
-import vuetify from './plugins/vuetify'
+
+// Import Vuetify and its styles
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// If you want to use mdi icons:
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const pinia = createPinia()
 pinia.use(piniaPersistedState)
