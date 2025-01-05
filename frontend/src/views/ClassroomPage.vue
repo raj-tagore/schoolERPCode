4328058642baf6dfc71b8e3aebce26d20d87269a
Raj Tagore <rajtagore780@gmail.com>
2025-01-05 20:55:28 +0530

    --

--- frontend/src/views/ClassroomPage.vue
+++ frontend/src/views/ClassroomPage.vue
@@ -5,15 +5,7 @@
 </template>
 
 <script>
-import { onMounted } from 'vue';
-import api from '@/services/api';
-
 export default {
     props: ['id'],
-    setup(props) {
-        onMounted(() => {
-
-        });
-    }
 }
 </script>
