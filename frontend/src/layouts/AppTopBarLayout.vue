30d86d7f7540d01d45ae7f63bf42066b09eedb46
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-01-08 19:36:36 +0530

    App top bar adjustments

--- frontend/src/layouts/AppTopBarLayout.vue
+++ frontend/src/layouts/AppTopBarLayout.vue
@@ -1,13 +1,13 @@
 <template>
 	<v-app>
-		<v-app-bar app color="grey">
+		<v-app-bar app color="grey" density="compact">
 			<v-toolbar-title class="flex text-center">
 				<h4>Classroom</h4>
 			</v-toolbar-title>
 		</v-app-bar>
 
 		<v-main>
-			<v-container class="mt-5">
+			<v-container>
 				<router-view></router-view>
 			</v-container>
 		</v-main>
