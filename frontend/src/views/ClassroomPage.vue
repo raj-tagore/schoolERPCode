fffe757d9d1ebc209365f7d8a607b4465be287ea
Raj Tagore <rajtagore780@gmail.com>
2025-01-08 07:07:30 +0530

    --

--- frontend/src/views/ClassroomPage.vue
+++ frontend/src/views/ClassroomPage.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container v-if="this.classroom">
 		<v-row align="center" justify="center">
-			<v-col cols="12" lg="4">
+			<v-col>
 				<v-card>
 					<v-tabs
 						background-color="deep-purple accent-4"
@@ -10,32 +10,53 @@
 						align-tabs="center"
 						v-model="tabs"
 					>
-						<v-tab>Info</v-tab>
+						<v-tab>Overview</v-tab>
 						<v-tab>Settings</v-tab>
+						<v-tab>Members</v-tab>
 					</v-tabs>
 				</v-card>
 				<v-tabs-window v-model="tabs">
 					<v-tabs-window-item>
-						<v-card>
-							<v-img 
-								:src="getRandomClassroomImage()" 
-								class="custom-img"
-							></v-img>
-							<v-card-title class="text-body-1">{{this.classroom.standard}} - {{this.classroom.name}}</v-card-title>
-							<v-card-subtitle>{{ this.classroom.class_teacher_details?.user?.first_name || "Loading..." }}</v-card-subtitle>
-							<v-card-text>
-								<v-container>
-									<v-row>
-										<v-col v-for="subject in subjects" :key="subject.id" cols="12" lg="4">
-											{{ subject.name }}
-										</v-col>
-									</v-row>
-								</v-container>
-							</v-card-text>
-						</v-card>
+						<v-row class="ma-4">
+							<v-col cols="12" lg="4">
+								<v-card>
+									<v-img 
+										:src="getRandomClassroomImage()" 
+										class="custom-img"
+									></v-img>
+									<v-card-title>
+										{{this.classroom.name}}
+										<p class="text-body-2 pb-4">
+											<b>Standard: </b>{{this.classroom.standard}} <br>
+											<b>Class Teacher: </b>{{ this.classroom.class_teacher_details?.user?.first_name || "Loading..." }}
+										</p>
+									</v-card-title>
+								</v-card>
+							</v-col>
+							<v-col cols="12" lg="4">
+								<v-card>
+									<v-card-title>Subjects</v-card-title>
+									<v-card-text>
+										<v-container>
+											<v-row>
+												<v-col v-for="subject in subjects" :key="subject.id" cols="6">
+													<v-card>
+														<v-card-title class="text-body-1">{{ subject.name }}</v-card-title>
+														<v-card-subtitle>{{ subject.teacher_details.user.first_name + ' ' + subject.teacher_details.user.last_name }}</v-card-subtitle>
+														<v-card-actions class="justify-center">
+															<v-btn>Enter</v-btn>
+														</v-card-actions>
+													</v-card>
+												</v-col>
+											</v-row>
+										</v-container>
+									</v-card-text>
+								</v-card>
+							</v-col>
+						</v-row>
 					</v-tabs-window-item>
 					<v-tabs-window-item>
-						<v-card>
+						<v-card class="ma-4 pa-4">
 							<v-card-title>Settings</v-card-title>
 							<v-card-text>
 								<v-row>
@@ -52,41 +73,43 @@
 							</v-card-text>
 						</v-card>
 					</v-tabs-window-item>
+					<v-tabs-window-item>
+						<v-row class="ma-4">
+							<v-col>
+								<v-card>
+									<v-card-title>
+										Teachers
+									</v-card-title>
+									<v-card-text>
+										<v-data-table :items="classroom.other_teachers" :headers="teacher_headers">
+											<template #[`item.id`]="{ item }">
+												<router-link :to="{ name: 'Dashboard', params: { id: item} }">
+													View Profile
+												</router-link>
+											</template>
+										</v-data-table>
+									</v-card-text>
+								</v-card>
+							</v-col>
+							<v-col>
+								<v-card>
+									<v-card-title>
+										Students
+									</v-card-title>
+									<v-card-text>
+										<v-data-table :items="classroom.students" :headers="student_headers">
+											<template #[`item.id`]="{ item }">
+												<router-link :to="{ name: 'Dashboard', params: { id: item } }">
+													View Profile
+													</router-link>
+											</template>
+										</v-data-table>
+									</v-card-text>
+								</v-card>
+							</v-col>
+						</v-row>
+					</v-tabs-window-item>
 				</v-tabs-window>
-			</v-col>
-		</v-row>
-		<v-row>
-			<v-col>
-				<v-card>
-					<v-card-title>
-						Teachers
-					</v-card-title>
-					<v-card-text>
-						<v-data-table :items="classroom.other_teachers" :headers="teacher_headers">
-							<template #[`item.id`]="{ item }">
-								<router-link :to="{ name: 'Dashboard', params: { id: item} }">
-									View Profile
-									</router-link>
-							</template>
-						</v-data-table>
-					</v-card-text>
-				</v-card>
-			</v-col>
-			<v-col>
-				<v-card>
-					<v-card-title>
-						Students
-					</v-card-title>
-					<v-card-text>
-						<v-data-table :items="classroom.students" :headers="student_headers">
-							<template #[`item.id`]="{ item }">
-								<router-link :to="{ name: 'Dashboard', params: { id: item} }">
-									View Profile
-									</router-link>
-							</template>
-						</v-data-table>
-					</v-card-text>
-				</v-card>
 			</v-col>
 		</v-row>
 	</v-container>
@@ -130,14 +153,13 @@ export default {
 		},
 		async getClassroomData() {
 			this.classroom = (await api.get(`api/allocation/classrooms/${this.id}`)).data;
-			this.subjects = (await api.get(`api/allocation/subjects/all?classroom=${this.id}`)).data;
+			this.subjects = (await api.get(`api/allocation/subjects/all/?classroom=${this.id}`)).data;
 			this.classroom.other_teachers = await Promise.all(this.classroom.other_teachers.map(async (teacher_id) => {
 				return (await api.get(`api/accounts/teachers/${teacher_id}/`)).data;
 			}));
 			this.classroom.students = await Promise.all(this.classroom.students.map(async (student_id) => {
 				return (await api.get(`api/accounts/students/${student_id}/`)).data;
 			}));
-			console.log(this.classroom);
 		},
 	},
 	mounted() {
