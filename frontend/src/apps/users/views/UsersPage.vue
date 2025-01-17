4c171057b546c679fc558cd29ea0c2dd35ff4ef2
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-01-17 14:07:00 +0530

    Teachers and Students in Users app

--- frontend/src/apps/users/views/UsersPage.vue
+++ frontend/src/apps/users/views/UsersPage.vue
@@ -1,13 +1,18 @@
 <template>
-<v-container>
-    <v-row>
-        <v-col cols="12" lg="4" md="6">
-            Lookup users component here
-        </v-col>
-    </v-row>
-</v-container>
+	<v-container>
+		<v-row>
+			<v-col cols="12" lg="6">
+				<TeachersTable />
+			</v-col>
+			<v-col cols="12" lg="6">
+				<StudentsTable />
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script setup>
+import StudentsTable from "@/apps/users/components/StudentsTable.vue";
+import TeachersTable from "@/apps/users/components/TeachersTable.vue";
 
 </script>
