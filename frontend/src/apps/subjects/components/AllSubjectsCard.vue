2a7918af258fbbedcc3bc938c0c624927c0697cf
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-01-17 13:06:16 +0530

    Search bar for subjects

--- frontend/src/apps/subjects/components/AllSubjectsCard.vue
+++ frontend/src/apps/subjects/components/AllSubjectsCard.vue
@@ -1,26 +1,55 @@
 <template>
-<v-container>
-    <v-card>
-        <v-row class="ma-2">
-            <v-col 
-            v-for="(classroom, index) in classroomsData" 
-            cols="12" lg="3" md="6"
-            :key="index">
-                <SubjectsList :filter="{ classroom: classroom.id }" :title="classroom.name"/>
-            </v-col>
-        </v-row>
-    </v-card>
-</v-container>
+	<v-container>
+		<v-card>
+			<v-card-text>
+				<v-container>
+					<v-row>
+						<v-col>
+						<v-text-field
+							v-model="search"
+							label="Search"
+						/>
+
+						</v-col>
+					</v-row>
+					<v-row class="ma-2">
+						<v-col 
+							v-for="(classroom, index) in classroomsData" 
+							cols="12" lg="3" md="6"
+							:key="index">
+							<SubjectsList :filter="{ classroom: classroom.id }" :title="classroom.name"/>
+						</v-col>
+					</v-row>
+
+				</v-container>
+			</v-card-text>
+		</v-card>
+	</v-container>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
 import { getClassrooms } from "@/apps/classrooms/api";
-import SubjectsList from "@/apps/subjects/components/SubjectsList"
+import SubjectsList from "@/apps/subjects/components/SubjectsList";
+import { onMounted, ref, computed } from "vue";
 
-const classroomsData = ref([])
+const classroomsDataRaw = ref([]);
+const search = ref("");
+
+const classroomsData = computed({
+	get: () => {
+		console.log(search.value.length);
+		if (search.value.length === 0) {
+			return classroomsDataRaw.value;
+		}
+		return classroomsDataRaw.value.filter((v) =>
+			`${v.name}${v.standard}`.includes(search.value),
+		);
+	},
+});
+
 const fetchClassroomsData = async () => {
-    classroomsData.value = await getClassrooms();
+	classroomsDataRaw.value = await getClassrooms();
+	console.log(classroomsDataRaw);
 };
 onMounted(fetchClassroomsData);
 </script>
