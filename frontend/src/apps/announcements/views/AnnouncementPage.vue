b5678ff05aa598c59fa2e902590ae3af400fa205
Raj Tagore <rajtagore780@gmail.com>
2025-01-21 11:30:43 +0530

    --

--- frontend/src/apps/announcements/views/AnnouncementPage.vue
+++ frontend/src/apps/announcements/views/AnnouncementPage.vue
@@ -1,35 +1,30 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-tabs background-color="deep-purple accent-4" center-active dark density="comfortable" align-tabs="center"
+            v-model="tabs">
+            <v-tab>View</v-tab>
+            <v-tab>Edit</v-tab>
+          </v-tabs>
+        </v-card>
+        <v-tabs-window v-model="tabs">
+          <v-tabs-window-item>
+            <v-row class="ma-2">
+              <v-col lg="6">
                 <v-card>
-                    <v-tabs
-                    background-color="deep-purple accent-4"
-                    center-active
-                    dark
-                    density="comfortable"
-                    align-tabs="center"
-                    v-model="tabs">
-                        <v-tab>View</v-tab>
-                        <v-tab>Edit</v-tab>
-                    </v-tabs>
+                  <v-card-title>{{ announcement?.title }}</v-card-title>
+                  <v-card-subtitle>{{ announcement?.signed_by?.user?.full_name }}</v-card-subtitle>
+                  <v-card-text>{{ announcement?.description }}</v-card-text>
                 </v-card>
-                <v-tabs-window v-model="tabs">
-                    <v-tabs-window-item>
-                        <v-row class="ma-2">
-                            <v-col lg="6">
-                            <v-card>
-                                <v-card-title>{{ announcement?.title }}</v-card-title>
-                                <v-card-subtitle>{{ announcement?.signed_by?.user?.full_name }}</v-card-subtitle>
-                                <v-card-text>{{ announcement?.description }}</v-card-text>
-                            </v-card>
-                            </v-col>
-                        </v-row>
-                    </v-tabs-window-item>
-                </v-tabs-window>
-            </v-col>
-        </v-row>
-    </v-container>
+              </v-col>
+            </v-row>
+          </v-tabs-window-item>
+        </v-tabs-window>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
@@ -39,17 +34,17 @@ import api from "@/services/api";
 let announcement = ref({});
 const tabs = ref(null);
 const props = defineProps({
-    announcementId: Number,
+  announcementId: Number,
 });
 
 const getAnnouncement = async () => {
-    announcement.value = (await api.get(`api/announcements/${props.announcementId}`)).data
+  announcement.value = (await api.get(`api/announcements/${props.announcementId}`)).data
 }
 
 onMounted(
-    async () => {
-        getAnnouncement();
-    }
+  async () => {
+    getAnnouncement();
+  }
 );
 
 </script>
--- frontend/src/apps/announcements/views/AnnouncementsPage.vue
+++ frontend/src/apps/announcements/views/AnnouncementsPage.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-container>
+  <v-container>
     <v-row>
-        <v-col cols="12" lg="4">
-            <AnnouncementsList :subtitle="'School Wide'" :url="'api/announcements/all/?is_school_wide=True'"/>
-        </v-col>
+      <v-col cols="12" lg="4">
+        <AnnouncementsList :subtitle="'School Wide'" :url="'api/announcements/all/?is_school_wide=True'" />
+      </v-col>
     </v-row>
-</v-container>
+  </v-container>
 </template>
 
 <script setup>
