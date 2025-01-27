69969a0523eb7455f77a6614fc66f27badeef874
Raj Tagore <rajtagore780@gmail.com>
2025-01-27 18:44:47 +0530

    --

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -9,7 +9,6 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
-        depth = 1
 
 class ParentSerializer(serializers.ModelSerializer):
     user = UserReadSerializer()
