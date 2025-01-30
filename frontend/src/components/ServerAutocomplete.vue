25cb5db6a96cec0bbb5051e1b1e1b5722c052ec5
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-01-30 11:23:27 +0530

    Added some docs to ServerAutocomplete

--- frontend/src/components/ServerAutocomplete.vue
+++ frontend/src/components/ServerAutocomplete.vue
@@ -7,12 +7,13 @@
 		:label="label"
 		:item-props="getInfo"
 		@input="selectedItem = $event.target.value"
+		<!--- update:modelValue is a special event that is emitted when you want to indicate to vue that model's value has been changed --->
 		@update:modelValue="emit('update:modelValue', $event)"
 		@update:search="debouncedFetchResults"
-		@click:clear="searchChanged"
+
 	>
 		<template v-slot:append-item>
-			<div v-if="hasMore" v-intersect="onIntersect" class="pa-4 teal--text">
+			<div v-if="hasMore" v-intersect="debouncedFetchResults" class="pa-4 teal--text">
 				Loading more items ...
 			</div>
 		</template>
@@ -46,13 +47,25 @@ const props = defineProps({
 	},
 });
 
+// Text used to search for items
 const query = ref("");
+
+// Filters passed to the fetch function
 const filters = ref({ page_size: 10, page: 1 });
+
 const results = ref([]);
+
 const selectedItem = ref(null);
+
+// To shut up the linter
+_ = selectedItem;
+
 const loading = ref(false);
+
+// Flag to indicate if there are more items to fetch
 const hasMore = ref(true);
 
+// For debouncing
 const triggered = ref(false);
 const reTriggered = ref(false);
 
@@ -75,6 +88,8 @@ const fetchResults = async () => {
 	}
 };
 
+// Allow for fetch every 300ms
+// Allow for re-triggering fetch if the user scrolls to the bottom of the list
 const debouncedFetchResults = () => {
 	if (triggered.value) {
 		reTriggered.value = true;
@@ -89,15 +104,6 @@ const debouncedFetchResults = () => {
 			debouncedFetchResults();
 		}
 	}, 300);
-};
-
-const searchChanged = () => {
-	hasMore.value = true;
-	debouncedFetchResults();
-};
-
-const onIntersect = () => {
-	debouncedFetchResults();
 };
 
 onMounted(fetchResults);
