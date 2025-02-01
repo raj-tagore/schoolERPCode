b09bc9e877ea73aed3e38baa8f5ed061f928c17a
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-02-01 16:35:17 +0530

    Using uv for managing python

--- backend/reset_db.sh
+++ backend/reset_db.sh
@@ -2,10 +2,9 @@
 
 cd "$(dirname "$0")"
 
-. ../venv/bin/activate
 
 
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.py" -not -name "*__init__*.py" -delete
 
 touch {tenants,attachments,assessments,assignments,attendance,accounts,users,announcements,allocation}/migrations/__init__.py
 
@@ -15,19 +14,27 @@ else
     SUPER="sudo"
 fi
 
+if command -v uv &> /dev/null; then
+    PYTHON_RUNNER="uv run"
+else
+	. ../venv/bin/activate
+    PYTHON_RUNNER="python"
+fi
+
 echo "Using: $SUPER"
+echo "Using: $PYTHON_RUNNER"
 
 $SUPER -u postgres psql -c "DROP DATABASE \"schoolERPDB\";"
 
 $SUPER -u postgres psql -c "CREATE DATABASE \"schoolERPDB\";"
 
-python manage.py makemigrations
+$PYTHON_RUNNER manage.py makemigrations
 
-python manage.py migrate
+$PYTHON_RUNNER manage.py migrate
 
-printf "public\nlocalhost\nlocalhost\n\n" | python manage.py create_tenant
+printf "public\nlocalhost\nlocalhost\n\n" | $PYTHON_RUNNER manage.py create_tenant
 
-printf "school1\nschool1\nschool1.localhost\n\n" | python manage.py create_tenant
+printf "school1\nschool1\nschool1.localhost\n\n" | $PYTHON_RUNNER manage.py create_tenant
 
 export DJANGO_SETTINGS_MODULE=schoolERPCode.settings
 
@@ -37,10 +44,10 @@ $SUPER -u postgres psql -c "INSERT INTO auth_group VALUES (2, 'Teacher')" "schoo
 $SUPER -u postgres psql -c "INSERT INTO auth_group VALUES (3, 'Student')" "schoolERPDB"
 $SUPER -u postgres psql -c "INSERT INTO auth_group VALUES (4, 'Parent')" "schoolERPDB"
 
-python import_dummy_data.py 2> /dev/null
+$PYTHON_RUNNER import_dummy_data.py 2> /dev/null
 
 export DJANGO_SUPERUSER_USERNAME=sandy
 export DJANGO_SUPERUSER_PASSWORD=san
 export DJANGO_SUPERUSER_EMAIL=san@san.com
 
-python manage.py createsuperuser --noinput
+$PYTHON_RUNNER manage.py createsuperuser --noinput
