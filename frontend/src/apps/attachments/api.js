9c4a0fcec2c23adfe129ad23ebaa5ba524fcc546
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-02-02 13:12:12 +0530

    Attachements get

--- frontend/src/apps/attachments/api.js
+++ frontend/src/apps/attachments/api.js
@@ -2,6 +2,11 @@
 const uploadAttachment = async (form) =>
     (await api.post("api/attachments/create", form)).data;
 
+const getAttachments = async (filters) =>
+    (await api.post("api/attachments/all", filters)).data;
+
+
 export {
 	uploadAttachment,
+	getAttachments,
 };
