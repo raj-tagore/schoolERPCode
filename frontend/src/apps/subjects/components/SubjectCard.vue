254310dbf047ca5c8413fdd34cf896db79274c46
Raj Tagore <rajtagore780@gmail.com>
2025-02-13 08:48:23 +0530

    cleanup and aesthetics

--- frontend/src/apps/subjects/components/SubjectCard.vue
+++ frontend/src/apps/subjects/components/SubjectCard.vue
@@ -1,7 +1,20 @@
 <template>
 	<v-card>
 		<v-card-title class="pb-0">{{subject.name}} ({{subject.classroom_details?.name}})</v-card-title>
-		<v-card-subtitle v-if="subject.teacher_details">{{ subject.teacher_details.user.full_name }}</v-card-subtitle>
+		<v-card-subtitle class="pt-2">
+			<v-chip 
+				v-if="subject.teacher_details"
+				link
+				:to="{ 
+					name: 'Teacher', 
+					params: { 
+						teacherId: subject.teacher_details?.id 
+					}
+				}"
+			>
+				Teacher: {{ subject.teacher_details?.user_details?.full_name }}
+			</v-chip>
+		</v-card-subtitle>
 		<v-card-text class="pb-4">
 			Overview: <p class="text-grey-darken-1">{{ subject.description }}</p>
 		</v-card-text>
--- frontend/src/components/FilterCard.vue
+++ frontend/src/components/FilterCard.vue
@@ -1,51 +1,55 @@
 <template>
-	<v-row>
-		<v-col cols="12" md="4" lg="3" v-for="field in filtersInfo">
-			<v-text-field 
-				v-if="field.type === 'string'"
-				:label="field.label" 
-				v-model="filters[field.key]"
-				:rules="[v => !!v || `${field.label} is required`]"
-				density="comfortable"
-				hide-details
-				:disabled="field.disabled"
-			></v-text-field>
-			<ServerAutocomplete
-				v-if="['number', 'classroom', 'subject', 'teacher'].includes(field.type)"
-				v-model="filters[field.key]"
-				:clearable="!field.disabled"
-				:fetch="getFilterFetch(field)"
-				:getInfo="getFilterInfo(field)"
-				:searchField="field.searchField || 'name'"
-				:label="field.label"
-				density="comfortable"
-				:disabled="field.disabled"
-			/>
-			<v-checkbox 
-				v-if="field.type === 'boolean'"
-				:label="field.label" 
-				v-model="filters[field.key]"
-			></v-checkbox>
-			<ServerAutocomplete
-				v-if="field.type === 'array'"
-				v-model="filters[field.key]"
-				clearable
-				:fetch="getFilterFetch(field)"
-				:getInfo="getFilterInfo(field)"
-				:searchField="field.searchField || 'name'"
-				:label="field.label"
-				:multiple='true'
-				density="comfortable"
-			/>
-			<v-select
-				v-if="field.type === 'n_nary'"
-				v-model="filters[field.key]"
-				:items="field.fetchOptions()"
-				:label="field.label"
-				hide-details
-				density="comfortable"
-			></v-select>
-			<v-date-input
+	<v-container
+		class="pa-4 bg-grey-lighten-5"
+		rounded="lg"
+	>
+		<v-row>
+			<v-col cols="12" md="4" lg="3" v-for="field in filtersInfo">
+				<v-text-field 
+					v-if="field.type === 'string'"
+					:label="field.label" 
+					v-model="filters[field.key]"
+					:rules="[v => !!v || `${field.label} is required`]"
+					density="comfortable"
+					hide-details
+					:disabled="field.disabled"
+				></v-text-field>
+				<ServerAutocomplete
+					v-if="['number', 'classroom', 'subject', 'teacher'].includes(field.type)"
+					v-model="filters[field.key]"
+					:clearable="!field.disabled"
+					:fetch="getFilterFetch(field)"
+					:getInfo="getFilterInfo(field)"
+					:searchField="field.searchField || 'name'"
+					:label="field.label"
+					density="comfortable"
+					:disabled="field.disabled"
+				/>
+				<v-checkbox 
+					v-if="field.type === 'boolean'"
+					:label="field.label" 
+					v-model="filters[field.key]"
+				></v-checkbox>
+				<ServerAutocomplete
+					v-if="field.type === 'array'"
+					v-model="filters[field.key]"
+					clearable
+					:fetch="getFilterFetch(field)"
+					:getInfo="getFilterInfo(field)"
+					:searchField="field.searchField || 'name'"
+					:label="field.label"
+					:multiple='true'
+					density="comfortable"
+				/>
+				<v-select
+					v-if="field.type === 'n_nary'"
+					v-model="filters[field.key]"
+					:items="field.fetchOptions()"
+					:label="field.label"
+					hide-details
+					density="comfortable"
+				></v-select>
+				<v-date-input
 				v-if="field.type === 'dates'"
 				color="primary"
 				:label="field.label" 
@@ -55,13 +59,14 @@
 				:disabled="field.disabled"
 				@update:modelValue="(value) => [...value].sort((a,b) => a - b).forEach((e, i) => {console.log(i, e);filters[field.key[i]] = e})"
 			></v-date-input>
-			<!--- The above one-liner does the following
-			- Gets the dates in an array from datepicker
-			- Sorts them, treating them as a number
-			- Assigns the corresponding date to the corresponding field in filter
-			--->
-		</v-col>
-	</v-row>
+				<!--- The above one-liner does the following
+				- Gets the dates in an array from datepicker
+				- Sorts them, treating them as a number
+				- Assigns the corresponding date to the corresponding field in filter
+				--->
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script setup>
